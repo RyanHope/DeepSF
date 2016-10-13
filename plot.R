19b316b0388a95fd06a9e4c5749745665d9b4e46
Ryan Hope <rmh3093@gmail.com>
2016-10-13 14:33:26 -0400

    plot updates

--- plot.R
+++ plot.R
@@ -1,125 +1,65 @@
-#setwd("C:/Users/Ryan/Dropbox/cmu-sf")
 setwd("~/Dropbox/cmu-sf/autoturn-data")
 
 require(data.table)
 require(ggplot2)
-require(grid)
-require(gridExtra)
 require(cowplot)
-source("multiplot.R")
 
-distance_reward <- function(x) {1-abs(max(min(x,180),40)-110)/70}
+sfplot <- function(d, vars, .ylab=NULL, .grouped=FALSE, .SE=FALSE, .k=5, .thresholds=NULL) {
+  leftpad <- unit(x=c(rel(1),rel(1),rel(1),rel(8)),units="mm")
+  if (.grouped)
+    p <- ggplot(d[variable %in% vars], aes(x=episode, y=value, color=factor(variable), group=variable)) + 
+      facet_wrap(~id, nrow=1, scales="free_x")
+  else
+    p <- ggplot(d[variable %in% vars], aes(x=episode, y=value)) + 
+      facet_grid(variable~id, scales="free")
+  if (!is.null(.thresholds))
+    p <- p + geom_hline((aes(yintercept=threshold)), data=.thresholds, color="magenta", alpha=.5)
+  if (!.SE)
+    p <- p + geom_line(size=.5, alpha=.5)    
+  p <- p + geom_smooth(se=.SE, method = "gam", formula = y ~ s(x, k = .k), size=.5) +
+    xlab("Episode") +
+    theme_bw()
+  if (!is.null(.ylab))
+    p <- p + ylab(.ylab)
+  if (.grouped)
+    p <- p + theme(plot.margin=leftpad) +
+      scale_color_brewer(palette="Set1") +
+      theme(legend.position=c(.01,.99),
+            legend.justification=c(0,1),
+            legend.title=element_blank(),
+            legend.text=element_text(size=7),
+            legend.key.size=unit(.5,"line"),
+            legend.background = element_rect(fill=alpha('black', 0.1)),
+            legend.margin=unit(0,"cm"))
+  p
+}
 
 sfplots <- function(.folder) {
-  d = rbindlist(lapply(list.files(path=.folder, pattern="*.tsv", full.names=TRUE), function(x) {.d=fread(x,header=T);if ("mean_eps" %in% names(x)) .d[,mean_eps:=NULL]; .d}))
-  .k = as.numeric(d[,ceiling(max(episode)/25)])
-  q = d[,.SD,.SDcols=c("id","episode","mean_q","maxscore","mean_absolute_error","outer_deaths","loss",
-                       "inner_deaths","episode_reward","shell_deaths","raw_pnts","resets","total",
+  d <- rbindlist(lapply(list.files(path=.folder, pattern="*.tsv", full.names=TRUE), function(x) {
+    .d <- fread(x,header=T)
+    if ("mean_eps" %in% names(x))
+      .d[,mean_eps:=NULL]
+    .d[,finalscore:=total]
+    .d
+  }))
+  .k <- max(as.numeric(d[,ceiling(max(episode)/25)]), 5)
+  q <- d[,.SD,.SDcols=c("id","episode","mean_q","maxscore","mean_absolute_error","outer_deaths","loss",
+                       "inner_deaths","episode_reward","shell_deaths","raw_pnts","resets","finalscore",
                        "fortress_kills","isi_pre","isi_post","reset_vlners")]
-  qm = melt(q,id.vars=c("episode","id"))
-  leftpad = unit(x=c(rel(1),rel(1),rel(1),rel(8)),units="mm")
-  plots = list(
-    p1 <- ggplot(qm[variable=="isi_pre"|variable=="isi_post"], aes(x=episode, y=value, color=factor(variable), group=variable)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      geom_hline((aes(yintercept=7.5))) +
-      ylab("Inter-shot interval") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p2 <- ggplot(qm[variable=="outer_deaths"|variable=="inner_deaths"|variable=="shell_deaths"], aes(x=episode, y=value, color=factor(variable), group=variable)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Deaths") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p3 <- ggplot(qm[variable=="total"|variable=="maxscore"][variable=="total",variable:="finalscore"][,], aes(x=episode, y=value, color=factor(variable), group=variable)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Score") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p4 <- ggplot(qm[variable=="resets"], aes(x=episode, y=value)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Resets") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p5 <- ggplot(qm[variable=="fortress_kills"], aes(x=episode, y=value)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Fortress Kills") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p6 <- ggplot(qm[variable=="raw_pnts"], aes(x=episode, y=value)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Raw Points") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p7 <- ggplot(qm[variable=="episode_reward"], aes(x=episode, y=value)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Reward") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p8 <- ggplot(qm[variable=="reset_vlners"], aes(x=episode, y=value)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Vlner at Reset") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top"),
-    p9 <- ggplot(qm[variable=="mean_q"], aes(x=episode, y=value)) + 
-      geom_line(size=.5, alpha=.5) +
-      facet_wrap(~id, nrow=1, scales="free_x") +
-      geom_smooth(se=FALSE, method = "gam", formula = y ~ s(x, k = .k)) +
-      ylab("Mean Q") +
-      xlab("Episode") +
-      theme_bw() +
-      theme(plot.margin=leftpad) +
-      scale_color_brewer(palette="Set1") +
-      theme(legend.position="top")
-  )
+  qm <- melt(q,id.vars=c("episode","id"))
+  
+  p1 <- sfplot(qm, c("isi_pre","isi_post"), .ylab="Inter-shot interval", .grouped=TRUE, .k=.k, .thresholds=data.table(variable="isi_pre",threshold=7.5))
+  p2 <- sfplot(qm, c("outer_deaths","inner_deaths","shell_deaths"), .ylab="Deaths", .grouped=TRUE, .k=.k)
+  p3 <- sfplot(qm, c("finalscore","maxscore"), .ylab="Score", .grouped=TRUE, .k=.k)
+  p6 <- sfplot(qm, c("mean_q","episode_reward","mean_absolute_error","loss"), .k=.k)
+  p7 <- sfplot(qm, c("resets","fortress_kills","raw_pnts","reset_vlners"), .k=.k)
   p.1 <- plot_grid(p1, p2, p3, labels=c("A","B","C"), align="v", ncol=1, hjust=-.5)
-  p.2 <- plot_grid(p8, p4, p5, p6, p7, p9, labels=c("D","E","F","G","H","I"), align="v", ncol=1, hjust=-.5)
+  p.2 <- plot_grid(p6, p7, labels=c("D","E"), align="v", ncol=1, hjust=-.5)
   plot_grid(p.1, p.2, align="h")
 }
 
 while (T) {
-  print(p <- sfplots("../deepsf-data/"))
-  # png("dqn-current.png", width = 1400, height = 900)
-  # print(p)
-  # dev.off()
+  .folder <- "../deepsf-data/"
+  print(p <- sfplots(.folder))
   Sys.sleep(120)
 }
